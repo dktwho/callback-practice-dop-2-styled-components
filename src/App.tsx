e14eefdd921ca9e2f4a3d5566576bdfbfba5ba58
Alexander Korobov <dktwho@mail.ru>
2023-07-08 17:21:55 +0300

    add hardcode todos array, create component todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {v4} from "uuid";
+import {Todolist} from "./components/Todolist";
+
+type TodosType = {
+    userId: string,
+    id: string,
+    title: string
+    completed: boolean
+}
 
 function App() {
+    let data = [
+        {userId: v4(), id: v4(), title: 'HTML', completed: true},
+        {userId: v4(), id: v4(), title: 'CSS', completed: true},
+        {userId: v4(), id: v4(), title: 'Vue', completed: false},
+        {userId: v4(), id: v4(), title: 'React', completed: true},
+        {userId: v4(), id: v4(), title: 'Redux', completed: false},
+    ]
+
+    let [todos, setTodos] = useState<TodosType[]>(data)
+
+    console.log(todos)
     return (
         <div className="App">
 
